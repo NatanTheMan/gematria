a3ae2e6a1f01a72ee294a1a2eaec4c897454f52a
Natan <natanscosta7@gmail.com>
2026-06-10 22:14:54 -0300

    feat: return all units with apostrophe

--- gematria.go
+++ gematria.go
@@ -1,5 +1,19 @@
 package gematria
 
+import "fmt"
+
 func Gematria(num int) string {
-	return "א׳"
+	units := []string{
+		"",
+		"א",
+		"ב",
+		"ג",
+		"ד",
+		"ה",
+		"ו",
+		"ז",
+		"ח",
+		"ט",
+	}
+	return fmt.Sprintf("%s׳", units[num])
 }
--- gematria_test.go
+++ gematria_test.go
@@ -2,17 +2,21 @@ package gematria
 
 import "testing"
 
-func TestGematria(t *testing.T) {
+func TestGematria_Units(t *testing.T) {
 	tests := []struct {
 		name string // description of this test case
 		num  int
 		want string
 	}{
-		{
-			"Units",
-			1,
-			"א׳",
-		},
+		{"Alef", 1, "א׳"},
+		{"Beit", 2, "ב׳"},
+		{"Guimel", 3, "ג׳"},
+		{"Dalet", 4, "ד׳"},
+		{"He", 5, "ה׳"},
+		{"Vav", 6, "ו׳"},
+		{"Zayn", 7, "ז׳"},
+		{"Chet", 8, "ח׳"},
+		{"Têt", 9, "ט׳"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
