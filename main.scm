eae4efd03f4bc11e34b442dc575e32f4b8da77d5
Natan <natanscosta7@gmail.com>
2026-06-28 22:01:03 -0300

    refactor: use better names

--- main.scm
+++ main.scm
@@ -3,42 +3,44 @@
         (chicken io)
         (chicken process-context))
 
-(define units (list "" "א" "ב" "ג" "ד" "ה" "ו" "ז" "ח" "ט"))
-(define dozens (list "" "י" "כ" "ל" "מ" "נ" "ס" "ע" "פ" "צ"))
-(define hundreds (list "" "ק" "ר" "ש" "ת"))
+(define units-list (list "" "א" "ב" "ג" "ד" "ה" "ו" "ז" "ח" "ט"))
+(define dozens-list (list "" "י" "כ" "ל" "מ" "נ" "ס" "ע" "פ" "צ"))
+(define hundreds-list (list "" "ק" "ר" "ש" "ת"))
 
-(define (uni n)
-  (list-ref units n))
+(define (to-unit n)
+  (list-ref units-list n))
 
-(define (dozen n)
+(define (to-dozen n)
   (cond 
     ((= n 15) "וט")
     ((= n 16) "זט")
-    (else (list-ref dozens (quotient n 10)))))
+    (else (list-ref dozens-list (quotient n 10)))))
 
-(define (hundred n)
-  (list-ref hundreds (quotient n 100)))
+(define (to-hundred n)
+  (list-ref hundreds-list (quotient n 100)))
 
 (define (gematria n)
-  (let ((d (remainder n 100)))
-    (if (< n 500)
-      (if (or (= d 16) (= d 15))
-        (string-append
-          (dozen d)
-          (hundred n))
-        (string-append
-          (uni (remainder n 10))
-          (dozen d)
-          (hundred n)))
+  (let ((d (remainder n 100))
+        (u (remainder n 10)))
 
-    "not supported")))
+    (if (or (= d 16) (= d 15))
+      (string-append
+        (to-dozen d)
+        (to-hundred n))
+
+      (string-append
+        (to-unit u)
+        (to-dozen d)
+        (to-hundred n)))))
 
 (define (main)
   (let* ((arg (car (command-line-arguments)))
-        (num (string->number arg)))
+         (num (string->number arg)))
 
     (if num
-      (display (gematria num))
+      (begin
+        (display (gematria num))
+        (newline))
       (display "not num"))))
 
 (main)
